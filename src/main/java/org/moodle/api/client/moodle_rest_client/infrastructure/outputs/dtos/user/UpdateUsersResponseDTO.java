6b49f5f91c26cd448aecdefbb81cc49257a4e0a1
JoseDevLab <jolevire1234@gmail.com>
2025-11-25 23:14:13 -0500

    Se completan correcciones de migracion en desarrollo.

--- src/main/java/org/moodle/api/client/moodle_rest_client/infrastructure/outputs/dtos/user/UpdateUsersResponseDTO.java
+++ src/main/java/org/moodle/api/client/moodle_rest_client/infrastructure/outputs/dtos/user/UpdateUsersResponseDTO.java
@@ -1,13 +1,13 @@
 package org.moodle.api.client.moodle_rest_client.infrastructure.outputs.dtos.user;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import org.moodle.api.client.moodle_rest_client.infrastructure.outputs.dtos.WarningDTO;
 
 import java.util.List;
 
 @Data
-@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class UpdateUsersResponseDTO {
     List<WarningDTO> warnings;
 }
