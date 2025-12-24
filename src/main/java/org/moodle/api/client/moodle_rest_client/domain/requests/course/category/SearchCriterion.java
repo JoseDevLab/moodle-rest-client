736a3047f852a12d80f0695a3d531fa99e58f92c
JoseDevLab <jolevire1234@gmail.com>
2025-12-24 18:50:46 -0500

    Se completa consulta de categorías a sincronizar por nivel educativo.

--- src/main/java/org/moodle/api/client/moodle_rest_client/domain/requests/course/category/SearchCriterion.java
+++ src/main/java/org/moodle/api/client/moodle_rest_client/domain/requests/course/category/SearchCriterion.java
@@ -6,6 +6,17 @@ import lombok.Data;
 @Data
 @Builder
 public class SearchCriterion {
+    /**
+     * The category column to search, expected keys (value format) are:"id" (int) the category id,"ids" (string) category ids separated by
+     * commas,"name" (string) the category name,"parent" (int) the parent category id,"idnumber" (string) category idnumber - user must have
+     * 'moodle/category:manage' to search on idnumber,"visible" (int) whether the returned categories must be visible or hidden. If the key is not passed,
+     * then the function return all categories that the user can see. - user must have 'moodle/category:manage' or
+     * 'moodle/category:viewhiddencategories' to search on visible,"theme" (string) only return the categories having this theme - user must have
+     * 'moodle/category:manage' to search on theme
+     */
     private String key;
+    /**
+     * the value to match
+     */
     private String value;
 }
