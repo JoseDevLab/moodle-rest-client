0c86725b45ee3de727828a35c9d97286f1f847f6
JoseDeveloper <jolevire1234@gmail.com>
2026-02-12 16:32:41 -0500

    Ya no se copian usuarios de las semillas. Se corrigen consultas de categorias y cursos.

--- src/main/java/org/moodle/api/client/moodle_rest_client/domain/requests/course/course/DuplicateCourseRequest.java
+++ src/main/java/org/moodle/api/client/moodle_rest_client/domain/requests/course/course/DuplicateCourseRequest.java
@@ -14,5 +14,20 @@ public class DuplicateCourseRequest {
     private String shortname;
     private Long categoryid;
     private Boolean visible;
+    /**
+     * name string is The backup option name:
+     * "activities" (int) Include course activites (default to 1 that is equal to yes),
+     * "blocks" (int) Include course blocks (default to 1 that is equal to yes),
+     * "filters" (int) Include course filters (default to 1 that is equal to yes),
+     * "users" (int) Include users (default to 0 that is equal to no),
+     * "enrolments" (int) Include enrolment methods (default to 1 - restore only with users),
+     * "role_assignments" (int) Include role assignments (default to 0 that is equal to no),
+     * "comments" (int) Include user comments (default to 0 that is equal to no),
+     * "userscompletion" (int) Include user course completion information (default to 0 that is equal to no),
+     * "logs" (int) Include course logs (default to 0 that is equal to no),
+     * "grade_histories" (int) Include histories (default to 0 that is equal to no)
+     * //
+     * value string is the value for the option 1 (yes) or 0 (no)
+     */
     private List<NameValueRequest> options;
 }
