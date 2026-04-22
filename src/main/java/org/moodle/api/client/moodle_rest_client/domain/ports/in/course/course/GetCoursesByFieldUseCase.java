527aa63f6289b5970156ce7fd960d266b6df704d
Jose Leonardo Villamizar Reyes <jolevire1234@gmail.com>
2026-04-22 16:02:34 -0500

    Se agrega asignacion de Operacion de Sincronizacion en la sincronizacion por matricula de estudiante.

--- src/main/java/org/moodle/api/client/moodle_rest_client/domain/ports/in/course/course/GetCoursesByFieldUseCase.java
+++ src/main/java/org/moodle/api/client/moodle_rest_client/domain/ports/in/course/course/GetCoursesByFieldUseCase.java
@@ -7,5 +7,19 @@ import org.moodle.api.client.moodle_rest_client.domain.models.MoodleCredentials;
 import java.util.List;
 
 public interface GetCoursesByFieldUseCase {
+    /**
+     *
+     * @param moodleCredentials
+     * @param field string  Valor por defecto para "" //The field to search can be left empty for all courses or:
+     *                     id: course id
+     *                     ids: comma separated course ids
+     *                     shortname: course short name
+     *                     idnumber: course id number
+     *                     category: category id the course belongs to
+     *                     sectionid: section id that belongs to a course
+     * @param value string  Valor por defecto para "" //The value to match
+     * @return
+     * @throws MoodleApiException
+     */
     List<Course> getCoursesByField(MoodleCredentials moodleCredentials, String field, String value) throws MoodleApiException;
 }
